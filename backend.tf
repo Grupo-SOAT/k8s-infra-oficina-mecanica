ae0692c99c8c8f20c3f649c75a66670acc1ec64a
Andre <andre.lui.tornice@gmail.com>
2026-08-15 17:45:09 -0300

    fix: update bucket tf state name grupo soat

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "oficina-mecanica-terraform-state"
+    bucket = "grupo-soat-oficina-mecanica-terraform-state"
     key    = "aws/terraform.tfstate"
     region = "us-east-1"
   }
