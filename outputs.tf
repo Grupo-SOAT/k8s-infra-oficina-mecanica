40e8ca5b5f72ccecdc731b05a806a9bda4327694
Leonardo <oziro56@gmai.com>
2026-09-09 21:13:06 -0300

    fix: step Configure kubectl

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "namespaces_criados" {
   value = module.namespaces.namespaces
 }
+
+output "cluster_name" {
+  description = "Nome do cluster EKS"
+  value       = module.eks.cluster_name
+}
